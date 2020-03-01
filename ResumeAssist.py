ef4e8da6c9607836e3ea9452de871d3dec7bffe7
Antonio Magana <39139814+AntonioMagana@users.noreply.github.com>
2020-02-29 18:56:13 -0800

    Add files via upload

---
+++ ResumeAssist.py
@@ -0,0 +1,123 @@
+from flask import render_template, redirect, url_for, flash, Flask, request
+from flask_login import current_user, login_required, login_user, logout_user
+from flask import Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import InputRequired, Email, Length, ValidationError
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_login import UserMixin
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Change this to be mySQL
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+#db = SQLAlchemy(app)
+
+#Decide if we want to use
+#bootstrap = Bootstrap(app)
+
+"""
+==================
+ResumeAssist.py
+==================
+"""
+
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Log In')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email(message="Enter your email"), Length(max=50)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=80)])
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Choose a different email.')
+
+@app.login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+#Update everything to be with mySQL
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    Todolist = db.relationship('Todo', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<user: {self.username}>'
+
+
+class Resume(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    contact = db.Column(db.String(200))
+    work = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<post: {self.text}>'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    return render_template('signup.html', form=form)
+
+
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html', name=current_user.username)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.username)
+
+@app.route('/newresume')
+@login_required
+def addevents():
+    return render_template('newresume.html', name=current_user.username)
+
+
+@app.route('/viewresumes')
+@login_required
+def reminders():
+    return render_template('viewresume.html', name=current_user.username)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run()
